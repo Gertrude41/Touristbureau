610d3e679849e8833bb475ec705eed9ca163cc5a
Gertrude Kingunza <gertrudekingunza7@gmail.com>
2023-11-03 11:59:03 -0500

    Hello

--- show_activities.js
+++ show_activities.js
@@ -2,4 +2,27 @@ console.log("hello")
 console.log(categories[0])
 for(i=0;i<categories.length;i++){
     console.log(categories[i])
+    cat.innerHTML+=`<option>${categories[i]}</option>`
+
+}
+function showActivities(e){
+    act.innerHTML=''
+    for(i=0;i<activities.length;i++){
+      if(activities[i].category==e.target.value){
+        act.innerHTML+=`<option value="${activities[i].id}">${activities[i].name}</option>`
+
+      } 
+    
+    }   
+}
+cat.onchange=showActivities
+function showExtra(e){
+    extra.innerHTML=''
+    for(i=0;i<activities.length;i++){
+      if(activities[i].id==e.target.value){
+        extra.innerHTML+=`${activities[i].id}">${activities[i].name}</option>`
+
+      } 
+    
+    }       
 }
